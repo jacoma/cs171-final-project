24b080c48b0c04a4fd0c684e5eceb39788dba208
anandramakris <47232702+anandramakris@users.noreply.github.com>
2019-11-24 15:24:38 -0500

    Added select box interaction

--- js/aquaculture.js
+++ js/aquaculture.js
@@ -1,88 +1,94 @@
-var width = 500;
-var height = 500;
+var aWidth = 350;
+var aHeight = 500;
 
 var svgAqua = d3.select("#aqua-chart").append("svg")
-    .attr("width", width)
-    .attr("height", height)
+    .attr("width", aWidth)
+    .attr("height", aHeight)
     .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    .attr("transform", "translate(" + aWidth / 2 + "," + aHeight / 2 + ")");
 
-d3.csv("data/aqua_timeseries.csv", function(data1) {
+var aquaText = svgAqua.append("g")
+    .append("text")
+    .attr("x", 0)
+    .attr("y", -200)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "24px")
+    .text("Aquaculture Per Year");
+
+var data1 = {};
+var data1display = {};
+var data2 = {};
+
+d3.csv("data/aqua_timeseries.csv", function(tData) {
     // convert selected values from string to numeric
-    data1.forEach(function(d) {
+    tData.forEach(function(d) {
         d.COUNTRY = +d.COUNTRY;
         d.YEAR = +d.YEAR;
         d.QUANTITY = +d.QUANTITY;
     });
 
-    d3.csv("data/aqua_countries.csv", function(data2) {
-        data2.forEach(function(d) {
+    d3.csv("data/aqua_countries.csv", function(cData) {
+        cData.forEach(function(d) {
             d.UN_Code = +d.UN_Code;
         });
 
-        // console.log(data1);
-        // console.log(data2);
+        console.log(tData);
+        console.log(cData);
 
-        // Filter by year
-        data1.filter(function(d) {
-            return d.YEAR === 2000;
-        });
+        data1 = tData;
+        data2 = cData;
 
-        // Add total quantity to second dataset
-        for (var i = 0; i < data2.length; i++) {
-            data2[i].Total_Quantity = 0;
-
-            for (var j = 0; j < data1.length; j++) {
-                if (data2[i].UN_Code === data1[j].COUNTRY) {
-                    data2[i].Total_Quantity += data1[j].QUANTITY;
-                }
-            }
-        }
-
-        var quantity = [];
-
-        for (var i = 0; i < data2.length; i++) {
-            quantity.push(data2[i].Total_Quantity);
-        }
-
-        // Pie chart initialization; WILL MOVE TO SEPARATE FUNCTION
-        var pie = d3.pie();
-
-        var colors = d3.scaleOrdinal(d3.schemeCategory10);
-
-        var outerRadius = width/2;
-        var innerRadius = 0;
-
-        var arc = d3.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius);
-
-        // console.log(quantity);
-
-        var chart = svgAqua.selectAll(".pie")
-            .data(pie(quantity))
-            .enter()
-            .append("g")
-            .attr("class", "pie");
-
-        chart.append("path")
-            .attr("fill", function(d, i) {
-                return colors(i);
-            })
-            .attr("d", arc);
-
-        chart.append("text")
-            .attr("transform", function(d) {
-                return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("text-anchor", "middle")
-            .attr("fill", "#fff")
-            .text(function(d, i) {
-                if (d.value > 50000000) {
-                    // console.log(d.value);
-
-                    return data2[i].Name_En;
-                }
-            });
+        updateAquaculture();
     });
 });
+
+function updateAquaculture() {
+    // Filter by year
+    data1display = data1.filter(function(d) {
+        return d.YEAR === +d3.select("#aqua-year").property("value");
+    });
+
+    // Add total quantity to second dataset
+    for (var i = 0; i < data2.length; i++) {
+        data2[i].Total_Quantity = 0;
+
+        for (var j = 0; j < data1display.length; j++) {
+            if (data2[i].UN_Code === data1display[j].COUNTRY) {
+                data2[i].Total_Quantity += data1display[j].QUANTITY;
+            }
+        }
+    }
+
+    var quantity = [];
+
+    for (var i = 0; i < data2.length; i++) {
+        quantity.push(data2[i].Total_Quantity);
+    }
+
+    // Pie chart initialization
+    var pie = d3.pie();
+
+    var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+    var outerRadius = aWidth/2;
+    var innerRadius = 0;
+
+    var arc = d3.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(outerRadius);
+
+    console.log(quantity);
+
+    var chart = svgAqua.selectAll(".pie")
+        .data(pie(quantity));
+
+    chart.exit().remove();
+
+    chart.enter()
+        .append("path")
+        .attr("fill", function(d, i) {
+            return colors(i);
+        })
+        .attr("d", arc)
+        .merge(chart);
+}
